a8309ed860a1698e351f9cb201fdc166bf287b3e
Yakhumov <dgahar.yakumov@gmail.com>
2023-09-15 20:20:53 +0300

    add eslint plugin

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:eslint-plugin/recommended",
+    "plugin:node/recommended",
+    "token", false 
+  ],
+  env: {
+    node: true,
+  },
+  overrides: [
+    {
+      files: ["tests/**/*.js"],
+      env: { mocha: true },
+    },
+  ],
+};
